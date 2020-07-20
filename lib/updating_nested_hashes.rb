24be0a09a953d16075b49ca83f4d15c7848a7d98
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-20 19:10:07 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -49,10 +49,6 @@ def add_characters
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
-      :additional_characters => [
-        {name: "Prince Escalus"},
-        {name: "Apothecary"}
-      ]
    }
    epic_tragedy
 end
